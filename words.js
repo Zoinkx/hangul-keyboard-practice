11d572249906ce3877b737507c2a79e62e329adc
Zoinkx <kimmygadegaardnielsen@live.dk>
2026-05-12 19:36:25 +0200

    Add Korean words and their translations

---
+++ words.js
@@ -0,0 +1,13 @@
+const levelBank = [
+    { word: "안녕하세요", translation: "(Hello)" },
+    { word: "떡볶이", translation: "(Spicy Rice Cakes)" },
+    { word: "감사합니다", translation: "(Thank you)" },
+    { word: "사과", translation: "(Apple)" },
+    { word: "고양이", translation: "(Cat)" },
+    { word: "우유", translation: "(Milk)" },
+    { word: "예쁘다", translation: "(Pretty)" },
+    { word: "컴퓨터", translation: "(Computer)" },
+    { word: "사랑해", translation: "(I love you)" },
+    { word: "물", translation: "(Water)" }
+    // Add as many as you want here!
+];
